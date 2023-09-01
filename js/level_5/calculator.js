f3f266207b9e84438439463265cfedd531b8b3df
ansanchexz23 <ansanchexz@gmail.com>
2023-08-31 21:49:40 -0500

    Update 31/08/23

---
+++ js/level_5/calculator.js
@@ -0,0 +1,28 @@
+// elementos del HTML
+const inputNumber = document.getElementById("inputNumber");
+// Button numbers
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+const btn4 = document.getElementById("btn4");
+const btn5 = document.getElementById("btn5");
+const btn6 = document.getElementById("btn6");
+const btn7 = document.getElementById("btn7");
+const btn8 = document.getElementById("btn8");
+const btn9 = document.getElementById("btn9");
+const btn0 = document.getElementById("btn0");
+// Operation buttons
+const btnDiv = document.getElementById("btnDiv");
+const btnMulti = document.getElementById("btnMulti");
+const btnresta = document.getElementById("btnresta");
+const btnSuma = document.getElementById("btnSuma");
+const btnPorcentaje = document.getElementById("btnPorcentaje");
+// Other buttons
+const btnCe = document.getElementById("btnCe");
+const btnC = document.getElementById("btnC");
+const btnDel = document.getElementById("btnDel");
+const btnIgual = document.getElementById("btnIgual");
+const btnPunto = document.getElementById("btnPunto");
+
+// onclick
+btn1.onclick = function () {};
