9a297a70d0b3aa50478e438209e3fa4c8c20f2a3
ansanchexz23 <ansanchexz@gmail.com>
2023-09-04 19:11:04 -0500

    Update 04/09/23

--- js/level_5/calculator_v2.js
+++ js/level_5/calculator_v2.js
@@ -4,4 +4,17 @@ const inputNumber = document.getElementById("inputNumber");
 let num1 = 0;
 let num2 = 0;
 let operator = "";
+
 // botones
+const buttons = [
+  { id: "btn1", value: "1" },
+  { id: "btn2", value: "2" },
+  { id: "btn3", value: "3" },
+  { id: "btn4", value: "4" },
+  { id: "btn5", value: "5" },
+  { id: "btn6", value: "6" },
+  { id: "btn7", value: "7" },
+  { id: "btn8", value: "8" },
+  { id: "btn9", value: "9" },
+  { id: "btn0", value: "0" },
+];
